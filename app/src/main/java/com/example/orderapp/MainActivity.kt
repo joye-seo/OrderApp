544d97c55411cba77cf390191d305dde166442db
joye <ssu1019@naver.com>
2022-05-10 16:14:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/orderapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.orderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setUpEvent()
+        setValue()
+    }
+
+
+    private fun setUpEvent(){
+
+
+
+    }
+    private fun setValue(){
+
+
+    }
+}
